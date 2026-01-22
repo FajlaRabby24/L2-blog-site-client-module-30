d0f12b8a56ba97028c4565c2b7eadffc8293b130
Fajla Rabby <fajlarabbyla24@gmail.com>
2026-01-22 23:38:14 +0600

    module-32 (practice-done)

--- blog-site-client-practice/src/app/(commomLayout)/page.tsx
+++ blog-site-client-practice/src/app/(commomLayout)/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
+import { authClient } from "../../lib/auth-client";
+
+export default async function Home() {
+  const { data: session } = await authClient.getSession();
+  console.log(session);
+
   return <div></div>;
 }
