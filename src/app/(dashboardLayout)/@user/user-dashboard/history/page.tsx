f911b3e1fcf659959d206716b228ecb7ca3f6a80
FajlaRabby24 <fajlarabbyla24@gmail.com>
2026-01-30 20:37:30 +0600

    35-10 Wrapping up PaginationControls component

--- src/app/(dashboardLayout)/@user/user-dashboard/history/page.tsx
+++ src/app/(dashboardLayout)/@user/user-dashboard/history/page.tsx
@@ -1,4 +1,5 @@
 import HistoryTable from "@/components/modules/user/history/HistoryTable";
+import PaginationControls from "@/components/ui/pagination-controls";
 import { blogService } from "@/services/blogService";
 
 const Histroypage = async ({
@@ -11,10 +12,19 @@ const Histroypage = async ({
   const response = await blogService.getBlogPosts({ page });
   const posts = response?.data?.data || [];
 
+  const pagination = response.data.pagination || {
+    limit: 5,
+    page: 1,
+    total: 0,
+    totalPage: 1,
+  };
+
   return (
     <div>
       <h1 className="text-2xl font-bold mb-6">Blog post history</h1>
       <HistoryTable posts={posts} />
+
+      <PaginationControls meta={pagination} />
     </div>
   );
 };
